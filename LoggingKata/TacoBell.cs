39df4604d24bc59c92e66c240f673430be04ca2a
Dr. Rubisco <76263371+thedocruby@users.noreply.github.com>
2022-05-17 12:38:42 -0400

    Add TacoBell class

---
+++ LoggingKata/TacoBell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggingKata
+{
+    internal class TacoBell : ITrackable
+    {
+        public TacoBell(double latitude, double longitude, string name)
+        {
+            Name = name;
+            var point = new Point();
+            point.Latitude = latitude;
+            point.
+        }
+
+        public string Name { get; set; }
+        public Point Location { get; set; }
+    }
+}
